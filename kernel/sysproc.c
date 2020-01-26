3edabc166b36bedb5796cd07fe47534cbad59622
Sudarshan Konge <sudk1896@gmail.com>
2020-01-26 21:52:42 +0530

    Increase process size by n bytes but don't allocate memory. Output matches

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,13 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  if (n > 0){
+	myproc()->sz += n;
+  }
+  else{
+    if(growproc(n) < 0)
+      return -1; 
+  }
   return addr;
 }
 
